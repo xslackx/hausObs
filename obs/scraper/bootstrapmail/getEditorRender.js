10e55ed6c65a369f22d2c6a57151503b6faac54d
Ruan <xslackx@live.fr>
2023-10-30 11:11:09 -0300

    create methods for web.py

--- obs/scraper/bootstrapmail/getEditorRender.js
+++ obs/scraper/bootstrapmail/getEditorRender.js
@@ -1,2 +1,6 @@
 btnHtml = document.getElementById("viewCode")
 docCompiled = document.getElementById("compiledEditor")
+
+if ( btnHtml.click() ){
+    return docCompiled.innerHTML
+}
--- obs/scraper/bootstrapmail/web.py
+++ obs/scraper/bootstrapmail/web.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,6 +11,24 @@ class EditorOnline():
         self.cfg = { **dotenv_values(".env.bootstrapmail") }
         self.website = self.cfg["URL"]
         self.driver = webdriver.Chrome()
+        self.selectors = [
+            "/html/body/form/div/div[1]/div[3]/a[1]",
+            "/html/body/div/div[1]/div/div/form/label[1]/input",
+            "/html/body/div/div[1]/div/div/form/label[2]/input",
+            "/html/body/div/div[1]/div/div/form/input[2]",
+        ]
         
     def auth(self):
+        self.driver.get(self.website)
+        sleep(3)
+        self.driver.find_element(By.XPATH, self.selectors[0]).click()
+        sleep(3)
+        self.driver.find_element(By.XPATH, self.selectors[1]).send_keys(self.cfg["U"])
+        self.driver.find_element(By.XPATH, self.selectors[2]).send_keys(self.cfg["P"])
+        self.driver.find_element(By.XPATH, self.selectors[3]).click()
+        
+    def out(self):
+        pass
+    
+    def model(self):
         pass

a982aa3a3fce61d48c0a4e79f220f239ea7eaaa2
Ruan <xslackx@live.fr>
2023-10-31 20:15:26 -0300

    add model and renew getEditorRender

--- obs/scraper/bootstrapmail/getEditorRender.js
+++ obs/scraper/bootstrapmail/getEditorRender.js
@@ -1,6 +1,22 @@
-btnHtml = document.getElementById("viewCode")
-docCompiled = document.getElementById("compiledEditor")
+function selectTemplate(args){
 
-if ( btnHtml.click() ){
-    return docCompiled.innerHTML
+    btnHtml = document.getElementById("viewCode")
+    docCompiled = document.getElementById("compiledEditor")
+    tmpl = document.getElementsByClassName('group text-white flex items-center space-x-2 hover:underline')
+    
+    for (let i = 0; i < tmpl.length; i++){
+        if(tmpl[i].innerText == args){
+            tmpl[i].click()
+            btnHtml.click()
+            
+            return docCompiled.innerHTML
+            
+        }
+    }
+
 }
+
+
+return selectTemplate(arguments[0])
+
+
--- obs/scraper/bootstrapmail/web.py
+++ obs/scraper/bootstrapmail/web.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from dotenv import dotenv_values
 
 class EditorOnline():
+    
     def __init__(self) -> None:
         self.cfg = { **dotenv_values(".env.bootstrapmail") }
         self.website = self.cfg["URL"]
@@ -17,18 +18,39 @@ class EditorOnline():
             "/html/body/div/div[1]/div/div/form/label[2]/input",
             "/html/body/div/div[1]/div/div/form/input[2]",
         ]
+        self.logout = [
+            "/html/body/form/div/div[1]/a",
+            "/html/body/div/div[2]/div/div/div/div/div[1]/a"
+        ]
+        
+        self.tmpl = [
+            "detected",
+            "welcome"    
+        ]
         
     def auth(self):
         self.driver.get(self.website)
-        sleep(3)
+        sleep(1)
         self.driver.find_element(By.XPATH, self.selectors[0]).click()
-        sleep(3)
+        sleep(1)
         self.driver.find_element(By.XPATH, self.selectors[1]).send_keys(self.cfg["U"])
         self.driver.find_element(By.XPATH, self.selectors[2]).send_keys(self.cfg["P"])
         self.driver.find_element(By.XPATH, self.selectors[3]).click()
         
     def out(self):
-        pass
+        self.driver.find_element(By.XPATH, self.logout[0]).click()
+        sleep(1)
+        self.driver.find_element(By.XPATH, self.logout[1]).click()
+        self.driver.quit()
     
-    def model(self):
-        pass
+    def model(self, saved):
+        self.check = False
+        self.nametmpl = saved
+        
+        for self.allow in self.tmpl:
+            if self.nametmpl == self.allow:
+                self.check = True
+    
+        if self.check:
+            self.body = self.driver.execute_script(open("obs/scraper/bootstrapmail/getEditorRender.js").read(), f"{self.nametmpl}")
+            return self.body

89b0bcd65160d42e9e742ff3eb72c53a30300027
Ruan <xslackx@live.fr>
2023-10-31 20:20:19 -0300

    remove unsed libs

--- obs/scraper/bootstrapmail/web.py
+++ obs/scraper/bootstrapmail/web.py
@@ -2,8 +2,6 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from dotenv import dotenv_values
 
 class EditorOnline():
